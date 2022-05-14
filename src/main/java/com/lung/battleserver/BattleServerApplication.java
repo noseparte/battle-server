df78bb964633a4a597ca998e00f8c4cab76299ef
noseparte <noseparte@aliyun.com>
2022-05-14 19:59:45 +0800

    Initial commit

---
+++ src/main/java/com/lung/battleserver/BattleServerApplication.java
@@ -0,0 +1,13 @@
+package com.lung.battleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lung/battleserver/BattleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lung.battleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
